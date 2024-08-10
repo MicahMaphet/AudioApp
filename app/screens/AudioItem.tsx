d314d6ad797d25480febf524d5ac4e4eb6acdc59
MicahMaphet <micahmaphet@gmail.com>
2024-08-10 19:36:40 -0400

    added AudioPlayer button to AudioItem screen and centered items

--- app/screens/AudioItem.tsx
+++ app/screens/AudioItem.tsx
@@ -1,9 +1,11 @@
 import { Image, View } from "react-native";
+import AudioPlayer from "../AudioPlayer";
 
 const AudioItem = () => {
     return (
-        <View>
+        <View style={{alignItems: 'center'}}>
             <Image source={require('../../assets/images/book.png')}></Image>
+            <AudioPlayer></AudioPlayer>
         </View>
     );
 }
