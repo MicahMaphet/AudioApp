65a65d9163012c53ed06cc43abc3cb6968c99bae
MicahMaphet <micahmaphet@gmail.com>
2024-08-10 17:45:04 -0400

    refactored export in app

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { Text, View, Image, ScrollView, StyleSheet } from "react-native";
 import Tail from './app/Tail';
 
-export default function App() {
+const App = () => {
   return (
     <View
       style={{
@@ -22,3 +22,5 @@ export default function App() {
     </View>
   );
 }
+
+export default App;
