d22035fbb03f3479f9b167ebe49a30cbaf9ca2db
MicahMaphet <micahmaphet@gmail.com>
2024-08-10 14:02:46 -0400

    audio player working

--- app/index.tsx
+++ app/index.tsx
@@ -13,13 +13,10 @@ export default function Index() {
         <Tail title="Story A"></Tail>
         <Tail title="Story B"></Tail>
         <Tail title="Story C"></Tail>
+        <Tail title="Banana"></Tail>
         <Tail title="Bunny Story" 
             description="This is a story about a bunny that does things. 
             He is not a very nice bunny and needs to do a thing a thing a thing. 
-            1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefg-hijklmnopqrstuvwxyz.
-            1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefg-hijklmnopqrstuvwxyz.
-            1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefg-hijklmnopqrstuvwxyz.
-            1234567890abcdefghijklmnopqrstuvwxyz1234567890abcdefg-hijklmnopqrstuvwxyz.
             "></Tail>
       </ScrollView>
     </View>
