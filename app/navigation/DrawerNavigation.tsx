e0c46329633ec2e861d3d41376a407f8ae2be62b
MicahMaphet <micahmaphet@gmail.com>
2024-08-24 18:24:52 -0400

    added images to the drawer items

--- app/navigation/DrawerNavigation.tsx
+++ app/navigation/DrawerNavigation.tsx
@@ -1,15 +1,35 @@
-import { useWindowDimensions } from "react-native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { useWindowDimensions, View } from "react-native";
+import { createDrawerNavigator, DrawerContentComponentProps, DrawerContentScrollView, DrawerItem } from "@react-navigation/drawer";
 import AudioList from "../screens/AudioList";
 import AudioItem from "../screens/AudioItem";
+import { Ionicons } from "@expo/vector-icons";
 
 const Drawer = createDrawerNavigator();
+const CustomDrawerContent = (props: DrawerContentComponentProps) => {
+    return (
+        <View>
+            <DrawerContentScrollView {...props}>
+                <DrawerItem
+                    label='Audio List'
+                    icon={() => <Ionicons name='book' />}
+                    onPress={() => props.navigation.navigate('Audio List')}
+                />
+                <DrawerItem
+                    label='Audio Item'
+                    icon={() => <Ionicons name='book-outline' />}
+                    onPress={() => props.navigation.navigate('Audio Item')}
+                />
+            </DrawerContentScrollView>
+        </View>
+    )
+}
 
 export default function DrawerNavigator() {
   const isLargeScreen = useWindowDimensions().width >= 768;
   return (
       <Drawer.Navigator
         initialRouteName="Audio List"
+        drawerContent={CustomDrawerContent}
         screenOptions={{
           drawerType: isLargeScreen ? 'permanent' : 'slide'
         }}
