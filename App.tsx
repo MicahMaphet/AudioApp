38def651736e3cb796da9a137692580136b80535
MicahMaphet <micahmaphet@gmail.com>
2024-08-24 18:01:08 -0400

    draw stayes open if large screen, updated naming and formatting

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { useWindowDimensions } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import AudioList from "./app/screens/AudioList";
@@ -6,15 +7,17 @@ import AudioItem from "./app/screens/AudioItem";
 const Drawer = createDrawerNavigator();
 
 const App = () => {
+  const isLargeScreen = useWindowDimensions().width >= 768;
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen
-          name="AudioList"
-          component={AudioList} />
-        <Drawer.Screen
-          name="AudioItem"
-          component={AudioItem} />
+      <Drawer.Navigator
+        initialRouteName="Audio List"
+        screenOptions={{
+          drawerType: isLargeScreen ? 'permanent' : 'slide'
+        }}
+      >
+        <Drawer.Screen name="Audio List" component={AudioList} />
+        <Drawer.Screen name="Audio Item" component={AudioItem} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
