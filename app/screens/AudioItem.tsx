6a050fd423f463fa10ab990c4e9792b8f3ad454d
MicahMaphet <micahmaphet@gmail.com>
2024-08-31 13:57:36 -0400

    added item image and title to Audio Item screen

--- app/screens/AudioItem.tsx
+++ app/screens/AudioItem.tsx
@@ -1,24 +1,29 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import AudioPlayer from "../AudioPlayer";
 import { useRoute, RouteProp } from "@react-navigation/native";
+import { TailProps } from "../Tail";
 
 const AudioItem = () => {
-    const route = useRoute<RouteProp<Record<string, { audio: string }>, string>>();
-    const { audio } = route.params;
+    const route = useRoute<RouteProp<Record<string, TailProps>, string>>();
+    const { title, image, audio } = route.params;
     return (
         <View style={{alignItems: 'center'}}>
-            <Ionicons style={styles.image} name='book-sharp' />
-            <AudioPlayer audio={audio}></AudioPlayer>
+            {image ? <Image source={{uri: 'http://localhost:3000/images/Wizard.png'}} style={styles.image} />
+                   : <Ionicons style={styles.image} name='book-sharp' />}
+            <Text>{title}</Text>
+            {audio ? <AudioPlayer audio={audio}></AudioPlayer>
+                   : <Text>{title} has no audio http://localhost:3000/images/{image}</Text>}
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     image: {
-      fontSize: 150,
-      marginRight: 10,
-    },
+        fontSize:200,
+        width: 200,
+        height: 200,
+      },
 });
-
+  
 export default AudioItem;
